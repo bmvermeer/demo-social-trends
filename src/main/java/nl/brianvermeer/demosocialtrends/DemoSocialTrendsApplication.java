5b511e6781942b57456ca2eb17a697f48b54c47d
bmvermeer <brianvermeer@snyk.io>
2021-08-05 11:30:29 +0200

    Initial commit

---
+++ src/main/java/nl/brianvermeer/demosocialtrends/DemoSocialTrendsApplication.java
@@ -0,0 +1,13 @@
+package nl.brianvermeer.demosocialtrends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSocialTrendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSocialTrendsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/brianvermeer/demosocialtrends/DemoSocialTrendsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.brianvermeer.demosocialtrends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSocialTrendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
